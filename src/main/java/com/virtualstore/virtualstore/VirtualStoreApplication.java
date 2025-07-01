17728e5d98f6a66ecd9e7583388b568e16d5b62f
Lucas Bomfim <bmfmlucas@gmail.com>
2025-06-30 22:18:34 -0300

    ¨teste¨

--- src/main/java/com/virtualstore/virtualstore/VirtualStoreApplication.java
+++ src/main/java/com/virtualstore/virtualstore/VirtualStoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VirtualStoreApplication {
-    //test
+    //test2
     public static void main(String[] args) {
         SpringApplication.run(VirtualStoreApplication.class, args);
     }
